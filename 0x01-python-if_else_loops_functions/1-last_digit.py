55a85c0b922b9365ff8fb9ad3573afa8701f7d98
Alb4Tr02 <834@holbertonschool.com>
2019-09-10 20:22:36 -0400

    fix negative numbers

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,13 +2,12 @@
 import random
 number = random.randint(-10000, 10000)
 lst_dig = int(str(number)[-1])
-sign = ""
 if number < 0 and lst_dig != 0:
-    sign = "-"
+    lst_dig *= -1
 if lst_dig == 0:
     msg = "and is 0"
 elif lst_dig < 6:
     msg = "and is less than 6 and not 0"
 else:
     msg = "and is greater than 5"
-print("Last digit of {:d} is {}{} {}".format(number, sign, lst_dig, msg))
+print("Last digit of {:d} is {} {}".format(number, lst_dig, msg))
