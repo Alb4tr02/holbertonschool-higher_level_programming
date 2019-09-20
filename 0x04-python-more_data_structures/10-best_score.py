abc61e8eff544d6a78b2e38daf593a2bf6bab577
Alb4Tr02 <834@holbertonschool.com>
2019-09-19 22:26:22 -0400

    10-best_score.py update

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return
+    b = list(a_dictionary.keys())[0]
+    a = a_dictionary[b]
+    for k, v in a_dictionary.items():
+        if v > a:
+            b = k
+    return b
