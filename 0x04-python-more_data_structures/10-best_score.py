3f5003165470926998a0732720738de83038649f
Alb4Tr02 <834@holbertonschool.com>
2019-09-19 22:30:50 -0400

    10-best_score.py fix not condition

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if a_dictionary is None:
-        return
+    if a_dictionary is None or not a_dictionary:
+        return None
     b = list(a_dictionary.keys())[0]
     a = a_dictionary[b]
     for k, v in a_dictionary.items():
