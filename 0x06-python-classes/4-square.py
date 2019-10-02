544a034416b835ce9d88e48263619909b5b24b74
Alb4Tr02 <834@holbertonschool.com>
2019-10-01 20:53:42 -0400

    4-square.py update

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+class Square:
+    __size = 0
+
+    def __init__(self, size=0):
+        if size is not None:
+            if type(size) != int:
+                raise TypeError("size must be an integer")
+            if size < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = size
+
+    def area(self):
+        return (self.__size * self.__size)
+
+    def size(self):
+        return self.__size
+
+    def _size(self, value):
+        if value is not None:
+            if type(value) != int:
+                raise TypeError("size must be an integer")
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+
+    size = property(size, _size)
