84d875fba1dc3356230a41a03a1f028b83d7c175
Alb4Tr02 <834@holbertonschool.com>
2019-10-22 15:25:54 -0400

    fix doc

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -4,7 +4,7 @@ class Base:
     __nb_objects = 0
 
     def __init__(self, id=None):
-    """Constructor"""
+        """Constructor"""
         if id is not None:
             self.id = id
         else:
