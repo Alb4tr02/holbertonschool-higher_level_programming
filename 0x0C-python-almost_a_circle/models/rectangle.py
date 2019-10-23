1f65cf3819192045412b9fb58c5db45e25059486
Alb4Tr02 <834@holbertonschool.com>
2019-10-22 22:16:03 -0400

    rectangle.py add geters and setters

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -13,3 +13,43 @@ class Rectangle(Base):
         self.__x = x
         self.__y = y
         super().__init__(id)
+
+    @property
+    def width(self):
+        """getter width"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """width setter"""
+        self.__width = width
+
+    @property
+    def height(self):
+        """getter height"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """width setter"""
+        self.__height = height
+
+    @property
+    def x(self):
+        """getter x"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """x setter"""
+        self.__x = x
+
+    @property
+    def y(self):
+        """getter y"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """y setter"""
+        self.__y = y
