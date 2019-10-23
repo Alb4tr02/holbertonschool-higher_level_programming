50878d5bca9a362a92b771f0f1ba7340ce3a05bc
Alb4Tr02 <834@holbertonschool.com>
2019-10-23 00:08:22 -0400

    rectangle.py update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,14 @@ class Rectangle(Base):
         """retrun str"""
         return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y,
                                                        self.width, self.height)
+
+    def update(self, *args):
+        """update the rectangle"""
+        try:
+            self.id = args[0]
+            self.__width = args[1]
+            self.__height = args[2]
+            self.__x = args[3]
+            self.__y = args[4]
+        except IndexError:
+            pass
