0409416f30ca6745d95e661df5ac13a713f4afd1
Alb4Tr02 <834@holbertonschool.com>
2019-10-24 11:42:07 -0400

    test_base.py por fin me di cuenta. >:v

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -30,3 +30,19 @@ class TestBaseClass(unittest.TestCase):
         Base.id = 34
         a9 = Base()
         self.assertEqual(a9.id, 2)
+    def test_medicuenta(self):
+        """Cases"""
+        a1 = Base(-32)
+        a2 = Base(-2)
+        a3 = Base(0)
+        a4 = Base(-0)
+        a5 = Base(32)
+        a6 = Base(3200)
+        a7 = Base(1)
+        self.assertEqual(a1.id, -32)
+        self.assertEqual(a2.id, -2)
+        self.assertEqual(a3.id, 0)
+        self.assertEqual(a4.id, 0)
+        self.assertEqual(a5.id, 32)
+        self.assertEqual(a6.id, 3200)
+        self.assertEqual(a7.id, 1)
