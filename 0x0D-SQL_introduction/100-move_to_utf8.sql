43f6e24c8f2b18b320a12cb814b1fcdaf4399471
Alb4Tr02 <834@holbertonschool.com>
2019-11-28 15:27:30 -0500

    100-move_to_utf8.sql update

---
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -0,0 +1,4 @@
+-- Db to UTF-8 and tables
+USE hbtn_0c_0;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
