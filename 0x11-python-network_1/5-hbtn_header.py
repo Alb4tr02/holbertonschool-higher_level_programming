c3db7ae47daf695ea7c127b43b3379573d2bbe89
Alb4Tr02 <834@holbertonschool.com>
2020-01-13 18:07:05 -0500

    5-hbtn_header.py except

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     req = requests.get(url)
     try:
-        print(req.headers['X-Request-Idsfgdsf'])
+        print(req.headers['X-Request-Id'])
     except KeyError:
         pass
