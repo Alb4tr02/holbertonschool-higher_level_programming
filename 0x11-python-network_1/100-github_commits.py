cf27979786ff39f6980a56dfb43c8b700ee070c4
Alb4Tr02 <834@holbertonschool.com>
2020-01-13 22:55:45 -0500

    100-github_commits.py update

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""  Python script that takes your Github credentials (username and password)
+     and uses the Github API to display your id """
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    url = "https://api.github.com/repos/"
+    user = sys.argv[1]
+    repo = sys.argv[2]
+    url = url + user + "/" + repo + "/commits"
+    req = requests.get(url)
+    json = req.json()
+    cnt = 0
+    for res in json:
+        print("{}: {}".format(res['sha'], res['commit']['author']['name']))
+        cnt = cnt + 1
+        if cnt > 9:
+            break
