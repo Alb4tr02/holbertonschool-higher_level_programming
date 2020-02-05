137d8e3e36239bb1ef5bae09124eb16a68ec4ed9
Alb4tr02 <834@holbertonschool.com>
2020-02-05 00:01:04 +0000

    6-multi_languages_loop.js update

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+
+const myVar0 = 'C is fun';
+const myVar1 = 'Python is cool';
+const myVar2 = 'Javascript is amazing';
+const arr = [myVar0, myVar1, myVar2];
+arr.forEach(element => console.log(element));
