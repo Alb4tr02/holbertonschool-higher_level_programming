b55a79e1bdb4a215c2d7c3d690c6ecbd8b8f642d
Alb4tr02 <834@holbertonschool.com>
2020-02-05 03:21:02 +0000

    11-second_biggest.js update

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+let first = Number(process.argv[2]);
+let second = Number(process.argv[3]);
+if (process.argv.length === 2 || process.argv.length === 3) {
+  console.log(0);
+} else {
+  for (let i = 2; i < process.argv.length; i++) {
+    if (Number(process.argv[i]) > first) {
+      second = first;
+      first = Number(process.argv[i]);
+    }
+  }
+}
+console.log(second);
