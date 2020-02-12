2104bf636aa7d2878c9aa0c46ecb12f7245c8b48
Alb4tr02 <834@holbertonschool.com>
2020-02-12 03:53:13 +0000

    4-starwars_count.js check argument

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,12 +1,14 @@
 #!/usr/bin/node
 const request = require('request');
-request(process.argv[2], function (error, response, body) {
-  const res = JSON.parse(body).results;
-  const cmp = 'https://swapi.co/api/people/18/';
-  let cnt = 0;
-  res.forEach(function (elmnt) {
-    if (elmnt.characters.includes(cmp)) cnt++;
+if (process.argv[2] !== undefined) {
+  request(process.argv[2], function (error, response, body) {
+    const res = JSON.parse(body).results;
+    const cmp = 'https://swapi.co/api/people/18/';
+    let cnt = 0;
+    res.forEach(function (elmnt) {
+      if (elmnt.characters.includes(cmp)) cnt++;
+    });
+    console.log(cnt);
+    if (error) {}
   });
-  console.log(cnt);
-  if (error) {}
-});
+}
